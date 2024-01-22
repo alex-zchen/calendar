adbfb66d4ffbf2a0e7fa42895e99532fe2df50de
planit <planit@tanjeffreyz.github.io>
2024-01-22 08:54:47 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -1,24 +1,107 @@
 assignments = {
-  "EECS 127/227AT-LEC-001": [
+  "ECE 110L.004.Sp24": [
     {
-      "title": "Homework 01 PDF",
-      "course": "EECS 127/227AT-LEC-001",
-      "dueDate": "2024-01-19T23:00:00",
-      "link": "https://www.gradescope.com/courses/699270",
+      "title": "HW 1",
+      "course": "ECE 110L.004.Sp24",
+      "dueDate": "2024-01-24T13:25:00",
+      "link": "https://www.gradescope.com/courses/711538",
+      "submitted": false
+    }
+  ],
+  "ECE 110L9.01L.Sp24": [
+    {
+      "title": "Lab 0 Assignment",
+      "course": "ECE 110L9.01L.Sp24",
+      "dueDate": "2024-01-23T21:00:00",
+      "link": "https://www.gradescope.com/courses/685697/assignments/3820101/submissions/222708652",
+      "submitted": true
+    },
+    {
+      "title": "Pre-Lab 1 Assignment",
+      "course": "ECE 110L9.01L.Sp24",
+      "dueDate": "2024-01-22T10:00:00",
+      "link": "https://www.gradescope.com/courses/685697/assignments/3820104/submissions/223650802",
+      "submitted": true
+    }
+  ],
+  "ECE/CS 250": [
+    {
+      "title": "Getting Started",
+      "course": "ECE/CS 250",
+      "dueDate": "2024-01-17T17:00:00",
+      "link": "https://www.gradescope.com/courses/696523/assignments/3874227/submissions/221996671",
+      "submitted": true
+    },
+    {
+      "title": "Homework 2: Unix",
+      "course": "ECE/CS 250",
+      "dueDate": "2024-01-17T17:00:00",
+      "link": "https://www.gradescope.com/courses/696523/assignments/3883046/submissions/222786955",
+      "submitted": true
+    },
+    {
+      "title": "Homework 1: Course Rules",
+      "course": "ECE/CS 250",
+      "dueDate": "2024-01-12T17:00:00",
+      "link": "https://www.gradescope.com/courses/696523/assignments/3902869/submissions/221998563",
+      "submitted": true
+    },
+    {
+      "title": "Homework 3: C (Code)",
+      "course": "ECE/CS 250",
+      "dueDate": "2024-02-02T17:00:00",
+      "link": "https://www.gradescope.com/courses/696523",
       "submitted": false
     },
     {
-      "title": "Homework 01 Self Grade",
-      "course": "EECS 127/227AT-LEC-001",
-      "dueDate": "2024-01-26T23:00:00",
-      "link": "https://www.gradescope.com/courses/699270/assignments/3973130/submissions/new",
+      "title": "Homework 3: C (Written)",
+      "course": "ECE/CS 250",
+      "dueDate": "2024-02-02T17:00:00",
+      "link": "https://www.gradescope.com/courses/696523",
       "submitted": false
+    }
+  ],
+  "Math 112L": [
+    {
+      "title": "Week 1 Check In",
+      "course": "Math 112L",
+      "dueDate": "2024-01-21T00:00:00",
+      "link": "https://www.gradescope.com/courses/694373/assignments/3881395/submissions/222176515",
+      "submitted": true
     },
     {
-      "title": "Homework 02 PDF",
-      "course": "EECS 127/227AT-LEC-001",
-      "dueDate": "2024-01-26T23:00:00",
-      "link": "https://www.gradescope.com/courses/699270",
+      "title": "Calculus 1 Review Lab",
+      "course": "Math 112L",
+      "dueDate": "2024-01-15T00:00:00",
+      "link": "https://www.gradescope.com/courses/694373/assignments/3910524/submissions/222155998",
+      "submitted": true
+    },
+    {
+      "title": "Problem Set #1",
+      "course": "Math 112L",
+      "dueDate": "2024-01-18T00:00:00",
+      "link": "https://www.gradescope.com/courses/694373/assignments/3922799/submissions/222932739",
+      "submitted": true
+    },
+    {
+      "title": "Week 2 Check In",
+      "course": "Math 112L",
+      "dueDate": "2024-01-23T00:00:00",
+      "link": "https://www.gradescope.com/courses/694373/assignments/3948716/submissions/223643108",
+      "submitted": true
+    },
+    {
+      "title": "Displacement & Total Distance Lab",
+      "course": "Math 112L",
+      "dueDate": "2024-01-19T00:00:00",
+      "link": "https://www.gradescope.com/courses/694373/assignments/3952207/submissions/223146332",
+      "submitted": true
+    },
+    {
+      "title": "Problem Set #2",
+      "course": "Math 112L",
+      "dueDate": "2024-01-25T00:00:00",
+      "link": "https://www.gradescope.com/courses/694373",
       "submitted": false
     }
   ]
