94f887002194df44b728c8f9f2c612406e1e45a7
planit <planit@tanjeffreyz.github.io>
2024-01-30 08:34:44 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -85,8 +85,8 @@ assignments = {
       "title": "Homework 3: C (Code)",
       "course": "ECE/CS 250",
       "dueDate": "2024-02-02T17:00:00",
-      "link": "https://www.gradescope.com/courses/696523",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/696523/assignments/3920935/submissions/225785274",
+      "submitted": true
     },
     {
       "title": "Homework 3: C (Written)",
