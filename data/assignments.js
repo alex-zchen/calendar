8e12474ffa848dc786ce9335e1d9b16568854dc4
planit <planit@tanjeffreyz.github.io>
2024-02-21 23:35:48 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -292,6 +292,13 @@ assignments = {
       "dueDate": "2024-02-22T00:00:00",
       "link": "https://www.gradescope.com/courses/694373/assignments/4124483/submissions/232736666",
       "submitted": true
+    },
+    {
+      "title": "Week 7 Check In",
+      "course": "Math 112L",
+      "dueDate": "2024-02-27T00:00:00",
+      "link": "https://www.gradescope.com/courses/694373/assignments/4124709/submissions/new",
+      "submitted": false
     }
   ]
 };
