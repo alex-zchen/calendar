ec75f8c4b99959dd30fadc9a9983affe795a3a0f
planit <planit@tanjeffreyz.github.io>
2024-02-25 13:35:02 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -171,6 +171,20 @@ assignments = {
       "dueDate": "2024-02-23T17:00:00",
       "link": "https://www.gradescope.com/courses/696523/assignments/4043536/submissions/233490501",
       "submitted": true
+    },
+    {
+      "title": "Homework 5: Logic (Code)",
+      "course": "ECE/CS 250",
+      "dueDate": "2024-03-04T17:00:00",
+      "link": "https://www.gradescope.com/courses/696523",
+      "submitted": false
+    },
+    {
+      "title": "Homework 5: Logic (Written)",
+      "course": "ECE/CS 250",
+      "dueDate": "2024-03-04T17:00:00",
+      "link": "https://www.gradescope.com/courses/696523",
+      "submitted": false
     }
   ],
   "Math 112L": [
