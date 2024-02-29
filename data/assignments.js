898064f851a282b7b9f4f0dc04fcf3edb1ebe6e4
planit <planit@tanjeffreyz.github.io>
2024-02-29 01:29:12 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -346,7 +346,7 @@ assignments = {
       "title": "Problem Set #6",
       "course": "Math 112L",
       "dueDate": "2024-02-29T00:00:00",
-      "link": "https://www.gradescope.com/courses/694373/assignments/4163870/submissions/235099759",
+      "link": "https://www.gradescope.com/courses/694373/assignments/4163870/submissions/235108524",
       "submitted": true
     }
   ]
