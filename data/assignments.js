3627d134ef5ac7cb733385f7fb60305e85ef2028
planit <planit@tanjeffreyz.github.io>
2024-03-18 19:34:33 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -48,6 +48,13 @@ assignments = {
       "dueDate": "2024-03-20T13:25:00",
       "link": "https://www.gradescope.com/courses/711538",
       "submitted": false
+    },
+    {
+      "title": "HW 8",
+      "course": "ECE 110L.004.Sp24",
+      "dueDate": "2024-03-27T13:25:00",
+      "link": "https://www.gradescope.com/courses/711538",
+      "submitted": false
     }
   ],
   "ECE 110L9.01L.Sp24": [
