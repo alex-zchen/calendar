d5be337dbbd9089a1bf6bc95ba3600294fde6aad
planit <planit@tanjeffreyz.github.io>
2024-03-19 14:34:36 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -66,6 +66,13 @@ assignments = {
       "submitted": true
     },
     {
+      "title": "IDC Week 4 Lab Assignment",
+      "course": "ECE 110L9.01L.Sp24",
+      "dueDate": "2024-03-26T21:00:00",
+      "link": "https://www.gradescope.com/courses/685697/assignments/3820097/submissions/new",
+      "submitted": false
+    },
+    {
       "title": "Lab 4 Assignment",
       "course": "ECE 110L9.01L.Sp24",
       "dueDate": "2024-02-20T21:00:00",
