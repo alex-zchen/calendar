2e18c8053ad5650e40bd38a8fd293a16433805e7
planit <planit@tanjeffreyz.github.io>
2024-03-28 20:34:57 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -465,8 +465,8 @@ assignments = {
       "title": "Taylor Series Review Lab",
       "course": "Math 112L",
       "dueDate": "2024-03-29T00:00:00",
-      "link": "https://www.gradescope.com/courses/694373",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/694373/assignments/4270990/submissions/242559586",
+      "submitted": true
     }
   ]
 };
