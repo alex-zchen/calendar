63b3eaabebc8ca302769c35eea43d57ba81dc110
planit <planit@tanjeffreyz.github.io>
2024-04-20 23:34:22 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -330,7 +330,7 @@ assignments = {
       "title": "Homework 7: Cache & Virtual Memory (Code)",
       "course": "ECE/CS 250",
       "dueDate": "2024-04-19T17:00:00",
-      "link": "https://www.gradescope.com/courses/696523/assignments/4307734/submissions/248290486",
+      "link": "https://www.gradescope.com/courses/696523/assignments/4307734/submissions/247962398",
       "submitted": true
     }
   ],
