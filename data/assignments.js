3991a6603bc6a6c7ed8991dbf1559684f9590c4c
planit <planit@tanjeffreyz.github.io>
2024-08-26 10:38:52 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -1,3 +1,60 @@
 assignments = {
-  "MATH 218D-2.001.Fa24": []
+  "MATH 218D-2.001.Fa24": [
+    {
+      "title": "Matrices and Vectors Quiz",
+      "course": "MATH 218D-2.001.Fa24",
+      "dueDate": "2024-09-03T23:30:00",
+      "link": "https://www.gradescope.com/courses/803996/assignments/4619775/submissions/new",
+      "submitted": false
+    },
+    {
+      "title": "Adjectives Quiz",
+      "course": "MATH 218D-2.001.Fa24",
+      "dueDate": "2024-09-03T23:30:00",
+      "link": "https://www.gradescope.com/courses/803996/assignments/4619776/submissions/new",
+      "submitted": false
+    },
+    {
+      "title": "Matrix-Vector Products Quiz",
+      "course": "MATH 218D-2.001.Fa24",
+      "dueDate": "2024-09-03T23:30:00",
+      "link": "https://www.gradescope.com/courses/803996/assignments/4619777/submissions/new",
+      "submitted": false
+    },
+    {
+      "title": "Digraphs Quiz",
+      "course": "MATH 218D-2.001.Fa24",
+      "dueDate": "2024-09-03T23:30:00",
+      "link": "https://www.gradescope.com/courses/803996/assignments/4619778/submissions/new",
+      "submitted": false
+    },
+    {
+      "title": "Matrices and Vectors Problem Set",
+      "course": "MATH 218D-2.001.Fa24",
+      "dueDate": "2024-09-03T23:30:00",
+      "link": "https://www.gradescope.com/courses/803996",
+      "submitted": false
+    },
+    {
+      "title": "Matrix-Vector Products Problem Set",
+      "course": "MATH 218D-2.001.Fa24",
+      "dueDate": "2024-09-03T23:30:00",
+      "link": "https://www.gradescope.com/courses/803996",
+      "submitted": false
+    },
+    {
+      "title": "Digraphs Problem Set",
+      "course": "MATH 218D-2.001.Fa24",
+      "dueDate": "2024-09-03T23:30:00",
+      "link": "https://www.gradescope.com/courses/803996",
+      "submitted": false
+    },
+    {
+      "title": "Welcome! Quiz",
+      "course": "MATH 218D-2.001.Fa24",
+      "dueDate": "2024-09-03T23:30:00",
+      "link": "https://www.gradescope.com/courses/803996/assignments/4810547/submissions/new",
+      "submitted": false
+    }
+  ]
 };
