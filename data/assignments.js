66e17606fb7bca8746a2ed0095c4350690710d44
planit <planit@tanjeffreyz.github.io>
2024-09-17 19:35:58 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -97,8 +97,8 @@ assignments = {
       "title": "Homework 3",
       "course": "ECE 350L.001.Fa24",
       "dueDate": "2024-09-17T23:59:00",
-      "link": "https://www.gradescope.com/courses/808240",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/808240/assignments/4914813/submissions/268335812",
+      "submitted": true
     },
     {
       "title": "CP2: Regfile",
