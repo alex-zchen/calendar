59b04a2aaa02207137672d9452801ea06149b188
planit <planit@tanjeffreyz.github.io>
2024-09-22 22:37:09 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -90,6 +90,13 @@ assignments = {
       "dueDate": "2024-09-27T18:00:00",
       "link": "https://www.gradescope.com/courses/842993",
       "submitted": false
+    },
+    {
+      "title": "HW3 Extra Credit",
+      "course": "CompSci 316 - Fall 2024",
+      "dueDate": "2024-09-27T18:00:00",
+      "link": "https://www.gradescope.com/courses/842993",
+      "submitted": false
     }
   ],
   "ECE 350L.001.Fa24": [
