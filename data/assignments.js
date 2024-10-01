cfd7e29f1d778fa45f4c8cbae4cf9f7b1e05bbf7
planit <planit@tanjeffreyz.github.io>
2024-10-01 22:38:35 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -379,8 +379,8 @@ assignments = {
       "title": "Null Spaces Problem Set",
       "course": "MATH 218D-2.001.Fa24",
       "dueDate": "2024-10-01T23:30:00",
-      "link": "https://www.gradescope.com/courses/803996",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/803996/assignments/4620676/submissions/273370226",
+      "submitted": true
     },
     {
       "title": "Eigenvalues Problem Set",
