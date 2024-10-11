52e68f99a05ac81b62ea79a77922256f0fee50bb
planit <planit@tanjeffreyz.github.io>
2024-10-11 16:42:10 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -116,7 +116,7 @@ assignments = {
       "title": "Discussion-section1-Quiz-Oct11",
       "course": "CompSci 316 - Fall 2024",
       "dueDate": "2024-10-11T12:00:00",
-      "link": "https://www.gradescope.com/courses/842993/assignments/5140704/submissions/new",
+      "link": "https://www.gradescope.com/courses/842993",
       "submitted": false
     }
   ],
