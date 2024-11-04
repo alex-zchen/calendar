84ae2bc9882fc22551eecd10caa80e79b61a3d7a
planit <planit@tanjeffreyz.github.io>
2024-11-04 21:35:35 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -195,6 +195,13 @@ assignments = {
       "dueDate": "2024-11-07T18:00:00",
       "link": "https://www.gradescope.com/courses/842993",
       "submitted": false
+    },
+    {
+      "title": "Homework  4 ",
+      "course": "CompSci 316 - Fall 2024",
+      "dueDate": "2024-11-07T18:00:00",
+      "link": "https://www.gradescope.com/courses/842993",
+      "submitted": false
     }
   ],
   "ECE 350L.001.Fa24": [
