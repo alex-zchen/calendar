a655b3d519590eae1fcb3496f6940b54c084c3fd
planit <planit@tanjeffreyz.github.io>
2024-11-08 06:43:10 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -318,6 +318,13 @@ assignments = {
       "submitted": true
     },
     {
+      "title": "CP4: CPU Technical Report (Optional)",
+      "course": "ECE 350L.001.Fa24",
+      "dueDate": "2024-11-14T23:59:00",
+      "link": "https://www.gradescope.com/courses/808240",
+      "submitted": false
+    },
+    {
       "title": "Homework 5",
       "course": "ECE 350L.001.Fa24",
       "dueDate": "2024-10-23T23:59:00",
