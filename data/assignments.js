b04467cb3c9f8e92ecfb4b903228832965417ba3
planit <planit@tanjeffreyz.github.io>
2024-11-18 11:35:30 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -670,6 +670,13 @@ assignments = {
       "submitted": false
     },
     {
+      "title": "Singular Value Decomposition Quiz",
+      "course": "MATH 218D-2.001.Fa24",
+      "dueDate": "2024-11-26T23:30:00",
+      "link": "https://www.gradescope.com/courses/803996/assignments/4620391/submissions/new",
+      "submitted": false
+    },
+    {
       "title": "Matrices and Vectors Problem Set",
       "course": "MATH 218D-2.001.Fa24",
       "dueDate": "2024-09-03T23:30:00",
@@ -890,6 +897,13 @@ assignments = {
       "title": "Definiteness Problem Set",
       "course": "MATH 218D-2.001.Fa24",
       "dueDate": "2024-11-19T23:30:00",
+      "link": "https://www.gradescope.com/courses/803996",
+      "submitted": false
+    },
+    {
+      "title": "Singular Value Decomposition Problem Set",
+      "course": "MATH 218D-2.001.Fa24",
+      "dueDate": "2024-11-26T23:30:00",
       "link": "https://www.gradescope.com/courses/803996",
       "submitted": false
     },
