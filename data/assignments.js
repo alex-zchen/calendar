c34d46752ec483f63132fb7335931f0c68075031
planit <planit@tanjeffreyz.github.io>
2024-12-10 23:36:40 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -454,8 +454,8 @@ assignments = {
       "title": "Project: Peer Evaluation",
       "course": "ECE 350L.001.Fa24",
       "dueDate": "2024-12-10T23:59:00",
-      "link": "https://www.gradescope.com/courses/808240/assignments/5190396/submissions/new",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/808240/assignments/5190396/submissions/296415880",
+      "submitted": true
     },
     {
       "title": "Technical Report",
