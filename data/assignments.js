999df488c35797159ec6a8aa22c13e200ca299cd
planit <planit@tanjeffreyz.github.io>
2025-01-07 15:07:47 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -1,5 +1,20 @@
 assignments = {
-  "ECE 230L9.01L.Sp25": [],
+  "ECE 230L9.01L.Sp25": [
+    {
+      "title": "Prelab 0",
+      "course": "ECE 230L9.01L.Sp25",
+      "dueDate": "2025-01-13T08:00:00",
+      "link": "https://www.gradescope.com/courses/929856/assignments/5476529/submissions/new",
+      "submitted": false
+    },
+    {
+      "title": "Lab 0 Mask Design",
+      "course": "ECE 230L9.01L.Sp25",
+      "dueDate": "2025-01-10T23:59:00",
+      "link": "https://www.gradescope.com/courses/929856/assignments/5476542/submissions/new",
+      "submitted": false
+    }
+  ],
   "ECE 280L9.01L.Sp25": [],
   "MATH 219.08.Sp25": []
 };
