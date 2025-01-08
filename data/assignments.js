f8b4c2b078c57eca0f61504c706b0bc57aca29c0
planit <planit@tanjeffreyz.github.io>
2025-01-08 19:35:23 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -10,7 +10,7 @@ assignments = {
     {
       "title": "Lab 0 Mask Design",
       "course": "ECE 230L9.01L.Sp25",
-      "dueDate": "2025-01-10T23:59:00",
+      "dueDate": "2025-01-17T23:59:00",
       "link": "https://www.gradescope.com/courses/929856/assignments/5476542/submissions/299137231",
       "submitted": true
     }
