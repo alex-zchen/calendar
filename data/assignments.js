91daebef25d24200adfcac812ccbd43a472597f8
planit <planit@tanjeffreyz.github.io>
2025-01-27 13:37:52 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -31,6 +31,13 @@ assignments = {
       "submitted": true
     },
     {
+      "title": "Lab 0 Part 3: Photolithography Data",
+      "course": "ECE 230L9.01L.Sp25",
+      "dueDate": "2025-02-07T23:59:00",
+      "link": "https://www.gradescope.com/courses/929856/assignments/5476530/submissions/new",
+      "submitted": false
+    },
+    {
       "title": "Lab 1 Orientation",
       "course": "ECE 230L9.01L.Sp25",
       "dueDate": "2025-01-31T23:59:00",
