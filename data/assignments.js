d1890fba3a76cfc1aeb8f45f19363f809e6082ab
planit <planit@tanjeffreyz.github.io>
2025-01-27 17:36:10 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -107,5 +107,13 @@ assignments = {
       "submitted": false
     }
   ],
-  "PHYSICS 152L.001.Sp25": []
+  "PHYSICS 152L.001.Sp25": [
+    {
+      "title": "Lab 1 Electrostatics and field mapping",
+      "course": "PHYSICS 152L.001.Sp25",
+      "dueDate": "2025-02-03T17:00:00",
+      "link": "https://www.gradescope.com/courses/948607",
+      "submitted": false
+    }
+  ]
 };
