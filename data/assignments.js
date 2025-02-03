00c37dca0d29efc03e5af1992f5b929fb6655428
planit <planit@tanjeffreyz.github.io>
2025-02-03 13:37:03 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -52,6 +52,13 @@ assignments = {
       "submitted": true
     },
     {
+      "title": "Lab 2 Van der Pauw Resistivity",
+      "course": "ECE 230L9.01L.Sp25",
+      "dueDate": "2025-02-21T23:59:00",
+      "link": "https://www.gradescope.com/courses/929856",
+      "submitted": false
+    },
+    {
       "title": "Lab 0 Mask Design",
       "course": "ECE 230L9.01L.Sp25",
       "dueDate": "2025-01-17T23:59:00",
