9210f82e7601ead84acf00ca9ad98706e53d2e27
planit <planit@tanjeffreyz.github.io>
2025-04-06 02:16:26 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -255,8 +255,8 @@ assignments = {
       "title": "ECE 280 Lab 6",
       "course": "ECE 280L9.01L.Sp25",
       "dueDate": "2025-04-05T21:00:00",
-      "link": "https://www.gradescope.com/courses/932061",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/932061/assignments/5490851/submissions/322797118",
+      "submitted": true
     },
     {
       "title": "ECE 280 Lab 1",
