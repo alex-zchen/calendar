70b215bc19f2784764ae6ff9637e7c17817bc8a3
planit <planit@tanjeffreyz.github.io>
2025-04-10 03:13:15 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -432,8 +432,8 @@ assignments = {
       "title": "Homework 12",
       "course": "MATH 219.08.Sp25",
       "dueDate": "2025-04-10T08:00:00",
-      "link": "https://www.gradescope.com/courses/937795",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/937795/assignments/6033818/submissions/324001809",
+      "submitted": true
     }
   ],
   "PHYSICS 152L.001.Sp25": [
