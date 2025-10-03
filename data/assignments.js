62d34b35c052db9b98fca8deccdb569003ebec26
alex-zchen <azc4@duke.edu>
2025-10-03 15:34:47 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -136,8 +136,8 @@ assignments = {
       "title": "Assignment 4",
       "course": "MATH 230.05 and 230.06 \u2014 Assignments",
       "dueDate": "2025-10-03T17:00:00",
-      "link": "https://www.gradescope.com/courses/1075503",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/1075503/assignments/6862675/submissions/354287682",
+      "submitted": true
     }
   ],
   "MATH 230.05 and 230.06 \u2014 Tests": [],
