64ecb70022364305a7fd1169398aeba0d9dbf2aa
alex-zchen <azc4@duke.edu>
2025-10-19 23:31:22 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -32,8 +32,8 @@ assignments = {
       "title": "Lab thread",
       "course": "COMPSCI 310.001.Fa25",
       "dueDate": "2025-10-22T23:59:00",
-      "link": "https://www.gradescope.com/courses/1087472",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/1087472/assignments/6905463/submissions/359913554",
+      "submitted": true
     }
   ],
   "ECE 270DL.001.Fa25": [
