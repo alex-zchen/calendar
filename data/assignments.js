224bb614203c13ac58f239e6d2baf9f4c3e7d443
alex-zchen <azc4@duke.edu>
2025-11-21 05:40:12 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -118,8 +118,8 @@ assignments = {
       "title": "ECE 270.Fa25 Homework9",
       "course": "ECE 270DL.001.Fa25",
       "dueDate": "2025-11-21T17:00:00",
-      "link": "https://www.gradescope.com/courses/1101321",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/1101321/assignments/7161897/submissions/372036000",
+      "submitted": true
     }
   ],
   "ECE 270L9.Fa25": [
