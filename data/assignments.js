3df9b218050c5dc238ad859667130eea5dba847c
alex-zchen <azc4@duke.edu>
2026-02-06 06:09:42 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -68,6 +68,13 @@ assignments = {
   ],
   "ECE 331L.001.Sp26": [
     {
+      "title": "PreLab 4: Differential Pair",
+      "course": "ECE 331L.001.Sp26",
+      "dueDate": "2026-02-23T08:00:00",
+      "link": "https://www.gradescope.com/courses/1206445",
+      "submitted": false
+    },
+    {
       "title": "Lab 1: Frequency-Domain and Time-Domain Measurements on RC Networks",
       "course": "ECE 331L.001.Sp26",
       "dueDate": "2026-01-22T22:00:00",
