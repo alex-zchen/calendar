1393544df586ad07fba00ffa946cab8892ab640d
alex-zchen <azc4@duke.edu>
2026-02-10 22:03:17 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -76,6 +76,13 @@ assignments = {
       "dueDate": "2026-02-17T23:59:00",
       "link": "https://www.gradescope.com/courses/1201561",
       "submitted": false
+    },
+    {
+      "title": "Homework 5 (Theory)",
+      "course": "CS 330",
+      "dueDate": "2026-02-17T23:59:00",
+      "link": "https://www.gradescope.com/courses/1201561",
+      "submitted": false
     }
   ],
   "CS/ECE 356": [
