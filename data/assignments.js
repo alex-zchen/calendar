8f3051855fccf7f56b88d2ebe75cdad16ca75d27
alex-zchen <azc4@duke.edu>
2026-04-07 03:34:38 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -164,7 +164,7 @@ assignments = {
     {
       "title": "Homework 10 (Theory)",
       "course": "CS 330",
-      "dueDate": "2026-04-07T23:59:00",
+      "dueDate": "2026-04-09T23:59:00",
       "link": "https://www.gradescope.com/courses/1201561",
       "submitted": false
     }
